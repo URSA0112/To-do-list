aadd5c51de63f0802ec25b6f575ea41e86457284
URSA0112 <Nimone99@gmail.com>
2025-02-04 00:24:25 +0800

    add function done dynamic

--- to-do-list/src/app/page.js
+++ to-do-list/src/app/page.js
@@ -1,34 +1,22 @@
 "use client";
 import { useState } from "react";
 import { useEffect } from "react";
-const plan2 = {
-  key: 2,
-  text: "cook",
-  done: true,
-}
-const inputvalues = [
-  {
-    key: 1, //(any)address for map , identify , search , find , ....
-    text: "take a shower", //(text)inputvalue
-    done: false, //(boolean)status ... for change
-  },
 
-];
 export default function Home() {
+
   const [inputtext, setInputtext] = useState("");
   const change = (event) => setInputtext(event.target.value);
-  const [lists, setLists] = useState(inputvalues);
+  const [saveinputvalue, setSaveinputvalue] = useState([])
 
-  const dynalist = lists.map((each, index) => <li key={index}>{each.text}</li>);
-  //const dynalists = lists.map((each)=>{return each})
+
+
   function add() {
-
- inputvalues.push(plan2)
-  
- console.log(lists.index)
-    return
-  } 
-  
+    if (saveinputvalue !== "")
+      setSaveinputvalue((saveinputvalue) => [...saveinputvalue, inputtext])
+    return console.log(saveinputvalue)
+  }
+  const list = saveinputvalue.map((each,index) => <li key={index}>{each}<button className="">X</button></li>)
+ 
 
 
   function clearText() {
@@ -44,8 +32,10 @@ export default function Home() {
           <button onClick={clearText}>CLEAR</button>
         </div>
         <div className="lists" style={{ display: "flex", width: "100%", gap: "20px" }}>
-          <ul className="eachlist-container">{dynalist}</ul>
-          <button className="">X</button>
+          <ul className="eachlist">
+            {list}
+          </ul>
+
         </div>
       </div>
     </>
